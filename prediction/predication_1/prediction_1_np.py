865e02b82d9fdea6f407359ae0a4dc77927523e8
artym <artym.surkov@mail.ru>
2024-04-30 16:40:32 +0500

    Разбиение по группам + картинки

--- prediction/predication_1/prediction_1_np.py
+++ prediction/predication_1/prediction_1_np.py
@@ -1,8 +1,6 @@
-#Несколько входов: полный выполняемый вход (с использованием NumPy)
+# Несколько входов: полный выполняемый вход (с использованием NumPy)
 
 import numpy as np
-
-weights = np.array([0.1, 0.2, 0])
 
 
 def neuralNetwork(input, weights):
@@ -13,6 +11,7 @@ def neuralNetwork(input, weights):
 avrGameOfPlayer = np.array([8.5, 9.5, 9.9, 9.0])
 wins = np.array([0.65, 0.8, 0.8, 0.9])
 nFans = np.array([1.2, 1.3, 0.5, 1.0])
+weights = np.array([0.1, 0.2, 0])
 
 input = np.array([avrGameOfPlayer[0], wins[0], nFans[0]])
 predication = neuralNetwork(input, weights)
--- prediction/predication_2/predication_2.py
+++ prediction/predication_2/predication_2.py
@@ -1,5 +1,10 @@
 # Прогнозирование с несколькими выходами
 
+def neuralNetwork(input, weighs):
+    pred = elemMultiplication(input, weighs)
+    return pred
+
+
 def elemMultiplication(num, vector):
     output = [0, 0, 0]
     assert (len(output) == len(vector))
@@ -8,13 +13,9 @@ def elemMultiplication(num, vector):
     return output
 
 
-def neuralNetwork(input, weighs):
-    pred = elemMultiplication(input, weighs)
-    return pred
-
-
-weights = [0.3, 0.2, 0.9]
 wins = [0.65, 0.8, 0.8, 0.9]
+weights = [0.3, 0.2, 0.9]
+
 input = wins[0]
 pred = neuralNetwork(input, weights)
 print(pred)
